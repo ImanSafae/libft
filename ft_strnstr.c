1a8f6ef5467f503ca0bd5a1d8282f94def492665
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-08-06 00:04:59 +0200

    Add files via upload

---
+++ ft_strnstr.c
@@ -0,0 +1,29 @@
+char	*ft_strnstr(char *s1, char *s2, size_t n)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	if (!s2)
+		return (s1);
+	while ((i < n) && s1)
+	{
+		if (s1[i] == s2[j])
+		{
+			k = i;
+			while (s1[i] == s2[j])
+			{
+				i++;
+				j++;
+			}
+		}
+		if (!s2[j])
+			return (s1 + k);
+		j = 0;
+		i++;
+	}
+	return (NULL);
+}
