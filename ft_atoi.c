726f7d8022208e1124175b4a01084bcdcd44fbb6
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-08-24 10:15:22 +0200

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <limits.h>
+
+static int	ft_strcmp(const char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while ((s1[i] == s2[i]) && s1[i] && s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	if (ft_strcmp(str, "-2147483648") == 0)
+		return (INT_MIN);
+	else if (ft_strcmp(str, "2147483647") == 0)
+		return (INT_MAX);
+	if (str[i] == "-")
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10;
+		res = res + (str[i] - 48);
+		i++;
+	}
+	return (res * sign);
+}
+
+int main()
+{
+	printf("%d", ft_atoi("-5464"));
+	return (0);
+}
