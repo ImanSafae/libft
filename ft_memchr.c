8d8f2ccea65e8621017dcad0b954921c0f325530
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-08-25 23:02:14 +0200

    Add files via upload

---
+++ ft_memchr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	int		i;
+	unsigned char	to_find;
+	unsigned char	*str;
+
+	i = 0;
+	to_find = (unsigned char)c;
+	*str = (unsigned char)*s;
+
+	while (i < n)
+	{
+		if (str[i] == to_find)
+			return (str + i);
+		i++;
+	}
+	return (NULL);
+}
