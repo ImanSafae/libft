44d97345281a9244b902942353ff0b653f438c6a
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-08-27 22:56:11 +0200

    Add files via upload

---
+++ ft_memcpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	int		i;
+	unsigned char	*destt;
+	unsigned char	*srcc;
+
+	destt = (unsigned char *)dest;
+	srcc = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		destt[i] = srcc[i];
+		i++;
+	}
+	return (dest);
+}
---
+++ ft_memset.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void	*ft_memset(void *dest, int src, size_t n)
+{
+	int 		i;
+	unsigned char	str;
+	unsigned char	*dst;
+
+	i = 0;
+	dst = (unsigned char *)dest;
+	str = (unsigned char)src;
+	while ((i < n) && dst[i])
+	{
+		dst[i] = str;
+		i++;
+	}
+	return (dest);
+}
