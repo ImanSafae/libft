e6f360359aab05f7465c3220da762677f6a20f80
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-08-27 22:55:06 +0200

    Add files via upload

---
+++ ft_memmove.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	int	i;
+	char	*dest;
+	char	*source;
+
+	i = 0;
+	dest = (char *)dst;
+	source = (char *)src;
+	if (dest > source)
+	{
+		while (i < len)
+		{
+			dest[len - i - 1] = source[len - i - 1];
+			i++;
+		}
+	}
+	else if (source > dest)
+	{
+		while (i < len)
+		{
+			dest[i] = source[i];
+			i++;
+		}
+	}
+	return (dst);
+}
