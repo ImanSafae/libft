223106e73da74d66f957ef61fb4ebce61c10176c
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-09-11 20:37:23 +0200

    Add files via upload

---
+++ ft_strtrim.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+static int	char_in_set(char const c, char const *set)
+{
+	int	index;
+
+	index = 0;
+	while (set[index])
+	{
+		if (set[index] == c)
+			return (1);
+		index++;
+	}
+	return (0);
+}
+
+static int	ft_strlen(char const *s)
+{
+	int	count;
+
+	count = 0;
+	while (s[count])
+		count++;
+	return (count);
+}
+
+char	*ft_strtrim(char const *s, char const *set)
+{
+	int	i;
+	int	j;
+	int	k;
+	char	*res;
+
+	i = 0;
+	k = 0;
+	while (s[i] && (char_in_set(s[i], set) == 1))
+		i++;
+	j = i;
+	i = (ft_strlen(s) - 1);
+	while (s[i] && char_in_set(s[i], set))
+		i--;
+	res = malloc(sizeof(char) * (i - j + 1));
+	if (!res)
+		return (NULL);
+	while (j <= i)
+	{
+		res[k] = s[j];
+		j++;
+		k++;
+	}
+	res[k] = '\0';
+	return (res);
+}
