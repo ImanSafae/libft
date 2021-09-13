6b1f334d3605ba93abe99f5215ed9887d9e404ef
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-09-13 22:31:48 +0200

    Add files via upload

---
+++ ft_strmapi.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+static int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char	*res;
+
+	i = 0;
+	res = malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (!res)
+		return (NULL);
+	while (s[i])
+	{
+		res[i] = f(i, s);
+		i++;
+	}
+	s[i] = '\0';
+	return (res);
+}
