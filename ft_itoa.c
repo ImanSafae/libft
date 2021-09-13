ae90c988a383cee8269b0106724820d24c48150f
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-09-13 22:14:15 +0200

    Add files via upload

---
+++ ft_itoa.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+
+static int	intlen(int n)
+{
+	int	count;
+	int	tmp;
+
+	count = 1;
+	tmp = n;
+	if (n < 0)
+	{
+		tmp = -n;
+		count++;
+	}
+	while (tmp >= 10)
+	{
+		tmp = (tmp / 10);
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*res;
+	int	i;
+
+	if (n == -2147483648)
+		return ("-2147483648");
+	else if (n == 2147483647)
+		return ("2147483647");
+	else if (n == 0)
+		return ("0");
+	res = malloc(sizeof(char) * (intlen(n) + 1));
+	i = intlen(n);
+	if (n < 0)
+	{
+		res[0] = '-';
+		n = -n;
+	}
+	res[i] = '\0';
+	i--;
+	while (n > 0)
+	{
+		res[i] = (n % 10) + 48;
+		i--;
+		n = (n / 10);
+	}
+	return (res);
+}
