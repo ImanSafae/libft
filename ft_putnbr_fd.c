734dc4d50fb58476aa5287c8b95b6f18ec2e410f
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-09-14 14:47:17 +0200

    Create ft_putnbr_fd.c

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void  ft_putnbr_fd(int n, int fd)
+{
+  if (n < 0)
+  {
+    write(fd, '-', 1);
+    n = -n;
+  }
+  if (n < 10)
+    write(fd, (n + 48), 1);
+  if (n >= 10)
+  {
+    ft_putnbr_fd((n / 10), fd);
+    write(fd, (n % 10), 1);
+  }
+}
