34a2e6575d88960882dcf1dc21ef8d1aef488d35
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-09-16 01:28:34 +0200

    Create ft_lstsize.c

---
+++ ft_lstsize.c
@@ -0,0 +1,16 @@
+int ft_lstsize(t_list *lst)
+{
+  int     count;
+  t_list  *counter;
+  
+  if (!lst)
+    return (0);
+  count = 1;
+  counter = lst->next;
+  while (counter != NULL)
+  {
+    count++;
+    counter = counter->next;
+  }
+  return (count);
+}
