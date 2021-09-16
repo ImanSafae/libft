57b7d062868b1c29fd359aa608e3e94ab126e96c
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-09-16 02:56:03 +0200

    Create ft_lstlast.c

---
+++ ft_lstlast.c
@@ -0,0 +1,13 @@
+t_list *ft_lstlast(t_list lst)
+{
+  t_list *pointer;
+  
+  if (!lst)
+    return (NULL);
+  pointer = lst->next;
+  while (pointer != NULL)
+  {
+    pointer = pointer->next; 
+  }
+  return (pointer);
+}
