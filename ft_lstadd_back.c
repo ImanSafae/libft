f1c1320a2acc3b079a56e63a32b09356ca9162c7
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-09-16 03:07:59 +0200

    Create ft_lstadd_back.c

---
+++ ft_lstadd_back.c
@@ -0,0 +1,11 @@
+void  ft_lstadd_back(t_list **alst, t_list *new)
+{
+  t_list  pointer;
+  
+  pointer = alst->next;
+  while (pointer != NULL)
+  {
+    pointer = pointer->next; 
+  }
+  pointer->next = new;
+}
