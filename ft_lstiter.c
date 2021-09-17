68ce4593448de364890c7e8edbac63b54a030703
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-09-17 23:37:39 +0200

    Create ft_lstiter.c

---
+++ ft_lstiter.c
@@ -0,0 +1,11 @@
+void  ft_lstiter(t_list *lst, void (*f)(*void))
+{
+  t_list pointer;
+  
+  pointer = lst;
+  while(pointer != NULL)
+  {
+    f(pointer->content);
+    pointer = pointer->next;
+  }
+}
