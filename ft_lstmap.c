d6a54a5eb8ec17e2c8a3ab7d44fce5117a8f4a67
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-09-20 23:28:43 +0200

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,4 +1,22 @@
 t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(*void))
 {
+   t_list   *newlist;
+   t_list   *i;
+   t_list   *j;
+   int      lst_len;
    
+   i = lst;
+   lst_len = ft_lstsize(lst);
+   newlist = malloc(sizeof(t_list) * lst_len);
+   if (!newlist)
+      return (NULL);
+   j = newlist;
+   while (i != NULL)
+   {
+      f(i->content);
+      j->content = i->content;
+      j = j->next;
+      i = i->next;
+   }
+   return (newlist);
 }
